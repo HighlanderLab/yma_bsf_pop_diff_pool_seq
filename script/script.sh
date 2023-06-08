3d38613e0ca896c98331d02445f8b7ebb163b598
beefhotp <y8672895@gmail.com>
2023-06-08 13:39:51 +0100

    preparing reference and mapping

--- script/script.sh
+++ script/script.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
+#$ -cwd
+#$ -N mapping
+#$ -l h_vmem=64G
 
-bwa index ../ref/ref.fasta
 
-bwa aln -n 0.01 -l 100 -o 1 -d 12 -e 12 -t 8 ../ref/ref.fasta ../sequence/SRR189066_1.fastq > ../map/SRR1.sai
-bwa aln -n 0.01 -l 100 -o 1 -d 12 -e 12 -t 8 ../ref/ref.fasta ../sequence/SRR189066_2.fastq > ../map/SRR2.sai
-bwa samse ../ref/ref.fasta ../map/SRR1.sai ../sequence/SRR189066_1.fastq > ../map/SRR1.sam
-bwa samse ../ref/ref.fasta ../map/SRR2.sai ../sequence/SRR189066_2.fastq > ../map/SRR2.sam
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
 
-#$ -o hello.o
-#$ -e hello.e
+module load roslin/bwa/2.1.0
+
+# load the reference file 
+#bwa index ../ref/ref.fasta 
+
+#map reads to reference
+bwa aln -n 0.01 -l 100 -o 1 -d 12 -e 12  ../ref/ref.fasta ../sequence/SRR189066_1.fastq > ../map/SRR1.sai
+bwa aln -n 0.01 -l 100 -o 1 -d 12 -e 12  ../ref/ref.fasta ../sequence/SRR189066_2.fastq > ../map/SRR2.sai
+
+bwa samse ../ref/ref.fasta ../map/SRR1.sai ../sequence/SRR189066_1.fastq > ../map/SSR1.sam
+bwa samse ../ref/ref.fasta ../map/SRR2.sai ../sequence/SRR189066_2.fastq > ../map/SSR2.sam
+
+#$ -e mapping.e
